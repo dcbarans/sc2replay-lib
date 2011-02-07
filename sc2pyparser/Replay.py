684c2383df64198517afd8ad73cdc34fcb5a8cc2
Dominic Baranski <dominic.baranski@gmail.com>
2011-02-07 02:23:44 -0500

    Code cleanup

--- sc2pyparser/Replay.py
+++ sc2pyparser/Replay.py
@@ -3,8 +3,6 @@ import StringIO
 
 from pprint import pprint
 
-DEBUG = True
-
 class Replay:
 
 	DETAILS_TYPE_BIN			= '\x02'
@@ -26,6 +24,8 @@ class Replay:
 				if(file_name == self.FILE_NAME_DETAILS):
 					(parsed_data, length) = self.parse_details(data)
 			
+					pprint(parsed_data)
+			
 		except IOError as (errno, strerror):
 			print strerror
 	
@@ -35,28 +35,18 @@ class Replay:
 	def parse_details(self, data):
 	
 		i = 0
-	
-		if DEBUG:
-			pprint(data[i])
-	
+		
 		# \x02 binary data
-		if(data[i] == self.DETAILS_TYPE_BIN):
-		
-			print 'found bin'
-		
+		if(data[i] == self.DETAILS_TYPE_BIN):		
 			i += 1
+			
 			(length, movement) = self.parse_number(data[i:])
 			i += movement + 1
-			
-			if DEBUG:
-				print "Bin: len(" + str(length) + ") = " + data[i : i + length]
-			
+						
 			return (data[i : i + length], i + length)
 			
 		# \x04 array
 		elif(data[i] == self.DETAILS_TYPE_ARRAY):
-		
-			print 'found array'
 		
 			# \x01 should follow identifyer...
 			i += 1
@@ -73,43 +63,27 @@ class Replay:
 			(length, movement) = self.parse_number(data[i:])
 			i += movement + 1
 			
-			if DEBUG:
-				print "Array: len(" + str(length) + ")"
-			
 			# move up i to the data!
 			j = 0
 			return_array = []
 			
 			while j < length:
-				
-				print 'array looping: ' + str(j) + " < " + str(length)
-				
+								
 				(new_data, movement) = self.parse_details(data[i:])				
 				i += movement
 				
 				return_array.append(new_data)
 				
-#				if data[i] != '\x02':
-#					raise Exception('Un-indexed array expected x02 after element, ' + str(ord(data[i])) + ' found')				
-#				i += 1
-
 				j += 1
 							
-			print "<<<<<< array-ing done"
-			pprint(return_array)
-
 			return (return_array, i)
 					
 		# \x05 Indexed array
 		elif(data[i] == self.DETAILS_TYPE_INDEXED_ARRAY):
 			i += 1
-			print 'found hash'
 			
 			(length, movement) = self.parse_number(data[i:])
 			i += movement + 1
-			
-			if DEBUG:
-				print "hash: len(" + str(length) + ")"
 			
 			return_array = []
 			nulls = 0
@@ -122,7 +96,6 @@ class Replay:
 				i += movement + 1
 				
 				while idx > (curr_idx):
-					print "Packing a 'None'"
 					return_array.append(None)
 					curr_idx += 1
 					nulls += 1
@@ -131,43 +104,27 @@ class Replay:
 				(new_data, movement) = self.parse_details(data[i:])				
 				i += movement
 				
-				return_array.append(new_data)
-				
-#				i += 1
+				return_array.append(new_data)				
 				curr_idx += 1
-				
-				print str(curr_idx) + " - " + str(nulls) + " < " + str(length)
 
-			print "<<<<<< hasing done"
-			pprint(return_array)
-			
 			return (return_array, i)
 		
 		elif(data[i] == self.DETAILS_TYPE_TINYINT):
-			i = i + 1
-			
-			print "Tiny int = " + str(ord(data[i]))
-			
+			i += 1			
 			return (ord(data[i]), i + 1)
 		
 		#\x07 Big Integer
 		elif(data[i] == self.DETAILS_TYPE_BIGINT):
-			i = i + 1
+			i += 1
 			
 			hex_str = hex(ord(data[i])) + str(hex(ord(data[i+1]))[2:]) + str(hex(ord(data[i+2]))[2:]) + str(hex(ord(data[i+3]))[2:])
-			
-			print "Big int = " + str(int(hex_str, 16))
 			
 			return (int(hex_str, 16), i+4)
 		
 		# \x09 VLF number found
 		elif(data[i] == self.DETAILS_TYPE_VLF):
-			i = i + 1
-			
-			if DEBUG:
-				(value, movement) = self.parse_number(data[i:])
-				print "Found VLF = " + str(value)
-			
+			i += 1
+						
 			(value, movement) = self.parse_number(data[i:])
 			return (value, i + movement+1)
 			
